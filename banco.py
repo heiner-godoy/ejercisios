aa61247f8a0cf655bd936e15680efcd99a0aa7d1
heiner-godoy <heiner.godoy@correounivalle.edu.co>
2023-10-22 14:56:46 -0500

    cambio

--- banco.py
+++ banco.py
@@ -2,70 +2,74 @@
 #  Las cuentas deben ser una lista de diccionarios, donde cada diccionario representa una cuenta con claves como numero_de_cuenta y saldo.
 #  Crea métodos para agregar cuentas, depositar y retirar dinero.
 
-class banco:
 
+class banco:
+    
     def __init__(self, nombre, ubicacion):
-        self.ubicacion = ubicacion
         self.nombre = nombre
+        self.ubicacion = ubicacion
         self.cuentas = []
 
-    def agregar_cuenta(self, numero_de_cuenta, saldo):
-        self.cuentas.append({"numero_de_cuenta": numero_de_cuenta, "saldo": saldo})
+    def agregar(self, numero_de_cuenta, claves, saldo):
+        self.cuentas.append({"numero_de_cuenta": numero_de_cuenta, "claves": claves, "saldo": saldo})
 
-
-    def mostrar_Saldo(self):
-            for cuenta in self.cuentas:
-                if cuenta["numero_de_cuenta"] == numero_de_cuenta:
-                    print(cuenta["saldo"])
-            return "Cuenta no encontrada"
-
-    def depositar(self, numero_de_cuenta, cantidad):
+    def depositar(self, numero_de_cuenta, claves, cantida):
         for cuenta in self.cuentas:
-            if cuenta["numero_de_cuenta"] == numero_de_cuenta:
-                cuenta["saldo"] += cantidad
-                return "Depósito exitoso"
-        return "Cuenta no encontrada"
+            if cuenta["numero_de_cuenta"] == numero_de_cuenta and cuenta["claves"] == claves:
+                cuenta["saldo"] += cantida
+                return "Deposito exitoso"
+        return "Cuenta no encontrada >> Revisa si tu contraseña o usuario son correctos <<"
 
-    def retirar(self, numero_de_cuenta, cantidad):
+    def retirar(self, numero_de_cuenta, claves, cantida):
         for cuenta in self.cuentas:
-            if cuenta["numero_de_cuenta"] == numero_de_cuenta:
-                if cuenta["saldo"] >= cantidad:
-                    cuenta["saldo"] -= cantidad
+            if cuenta["numero_de_cuenta"] == numero_de_cuenta and cuenta["claves"] == claves:
+                if cuenta["saldo"] >= cantida:
+                    cuenta["saldo"] -= cantida
                     return "Retiro exitoso"
                 else:
                     return "Saldo insuficiente"
-        return "Cuenta no encontrada"
+        return "Cuenta no encontrada >> Revisa si tu contraseña o usuario son correctos <<"
 
-mi_banco = banco("Banco tura", "Colombia")
+    def mostrar(self, numero_de_cuenta, claves):
+        for cuenta in self.cuentas:
+            if cuenta["numero_de_cuenta"] == numero_de_cuenta and cuenta["claves"] == claves:
+                print(cuenta["saldo"])
+                return
+        return "Cuenta no encontrada >> Revisa si tu contraseña o usuario son correctos <<"
+
+mi_banco = banco("Bancolombia", "Colombia")
 
 while True:
     print("1. Agregar cuenta")
     print("2. Depositar")
     print("3. Retirar")
-    print("4. Salir")
-    print("5 mostrar saldo")
+    print("4. Mostrar saldo")
+    print("5. Salir")
     opcion = input("Elige una opción: ")
 
-    if opcion == "1":
-        numero_de_cuenta = input("Ingresa el número de cuenta: ")
-        saldo = float(input("Ingresa el saldo inicial: "))
-        mi_banco.agregar_cuenta(numero_de_cuenta, saldo)
-    elif opcion == "2":
-        numero_de_cuenta = input("Ingresa el número de cuenta: ")
-        cantidad = float(input("Ingresa la cantidad a depositar: "))
-        print(mi_banco.depositar(numero_de_cuenta, cantidad))
+    if (opcion == "1"):
+        numero_de_cuenta = int(input("Tu id sera tu numero de cuenta a crear; Ingresa tu id: "))
+        claves = input("Crear clave: ")
+        saldo = int(input("Ingresa el saldo inicial: "))
+        mi_banco.agregar(numero_de_cuenta, claves, saldo)
+        print("Registro exitoso")
+
+    elif (opcion == "2"):
+        numero_de_cuenta = int(input("Ingresa numero de cuenta: "))
+        claves = input("Ingresa tu clave: ")
+        cantida = int(input("Ingresa el valor a depositar: "))
+        print(mi_banco.depositar(numero_de_cuenta, claves, cantida))
+
     elif opcion == "3":
-        numero_de_cuenta = input("Ingresa el número de cuenta: ")
-        cantidad = float(input("Ingresa la cantidad a retirar: "))
-        print(mi_banco.retirar(numero_de_cuenta, cantidad))
+        numero_de_cuenta = int(input("Ingresa numero de cuenta: "))
+        claves = input("Ingresa tu clave: ")
+        cantida = int(input("Ingresa el valor a retirar: "))
+        print(mi_banco.retirar(numero_de_cuenta, claves, cantida))
 
     elif opcion == "4":
-        numero_de_cuenta = input("Ingresa el número de cuenta: ")
-        mi_banco.mostrar_Saldo()
+        numero_de_cuenta = int(input("Ingresa numero de cuenta: "))
+        claves = input("Ingresa tu clave: ")
+        print(mi_banco.mostrar(numero_de_cuenta, claves))
 
     elif opcion == "5":
         break
-
-
-
-
